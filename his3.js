ad08d652123f861ff884edd7cf774d7907673d97
hussienail <63175073+hussienail@users.noreply.github.com>
2020-05-16 04:21:15 +0300

    Update his3.js

--- his3.js
+++ his3.js
@@ -1,22 +1,21 @@
 $(` <center><marquee direction="right" rfdقwidth="99%" id="asim55" onmouseover="this.stop()" onmouseout="لذthis.start()" scrolldelay="0" scrollamount="2" >قوانين الشات ● • في حال ذكر وتبيين اسماء مواقع اخرى تعرضك لحذف العضوية والحظر يرجى من الزوار والمراقبين عدم ذكر اسماء مواقع اخرى ● • وشكرآ لكم</marquee></font><center>
 `).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary');
 
-														$(`<img src="https://d.top4top.io/p_1596az4nu1.jpg
-" class="fr"
-style="width:100%;margin-right: 2px;">
-<a href="https:///store/apps/details?id=com.ramyosman.iraqachat" style="border-radius: 3px 12px 11px 11px;width:83px;margin:3px;" class="fl mini  fa fa-star-half-o btn btn-primary"> الإدارة</a> <a href="" style="width:85px;margin:2px; border-radius:20px 0 0" class="fl mini fa fa-heartbeat btn btn-primary"> إلتطبيق</a> <a href="sub.html" style="width:85px;margin:2px; border-radius: 0 0 20px" class="fl mini fa fa-star btn btn-primary"> ميز نفسك</a> <a href=" muhd" style="width:85px;margin:2px; border-radius: 0 20px" class="fl mini fa fa-graduation-cap btn btn-primary"> القوانين</a>
-<div style=" width: 100%; border-radius:0 0 20px 20px; background-color:DB5151" class="btn btn-primary btn-sm"type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria- fa-share"></div></></div>`).insertBefore('.nav-tabs');
-$(".border.corner").css("border-radius","24px 24px 24px 24px");
-$(".border.corner").css("border-radius","15px 3px 15px 15px")
-$(".fa-gear").text("الاعدادات ").css("width","")
-$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
-<link rel="stylesheet" href="https://raw.githack.com/iqr30/my-css-js-html/master/as2.css">
-<style>
-<style>`).insertBefore('body');
-$(` <center><div style=" width: 100%;height: auto!important; "  id="sa" margin: 2px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● •( إضغط هنا واستمع للراديو)• ● &nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>راديو المحبه</p><audio controls loop="" play=""><source src="https://listen.radioking.com/radio/96766/stream/136030"></audio></div><center>`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary')
-$('.break.light').css({"background-image":"url(https://k.top4top.io/p_1596291o11.gif"});
-$(`<center><font size="4" color="#C74653"> <marquee direction="right" width="100%" height="20" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;"> مرحباً بك ضيفنا الكريم في شات المحبه ، من أجل راحتك، شات المحبه  يجمع بين الجميع، 
- شكراً لك على تعاونك معنا، ونتمنى لك يوماً سعيداً - كل عام وانتم بخير  👋</marquee></font><center>`).insertBefore('#d2');
+ $(`<a  style="margin-top:-20px;padding :4px;float:right;border-radius:100px;" class="btn d btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(`<img src ="https://d.top4top.io/p_1596az4nu1.jpg" etclass="fr" style="width:100%;margin-top: 1px;">
+</div>
+<div id="Hos"><center><a id="tof9" href="https://gonative.io/manage/5nktn" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="Hos" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
+<a id="tof3" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="tof3" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p> حسين <i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i>قريبا</p> حسين <i class="fa fa-comment"></i> @asqw32 <i class="fa fa-share"></i> قريبا </p> soon <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> soon </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="https://c.top4top.io/p_1471yyke01.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$(`
+<link rel="stylesheet" href="https://raw.githack.com/iqr30/my-css-js-html/master/as2.css"><style>
+</style>`).insertBefore('body'),$(`
+<script type="text/javascript" src="https://iqr30.github.io/my-css-js-html/\u0634\u0627\u062A \u0639\u0631\u0627\u0642\u0646\u0627\u0627\u0627.js" />
+`).insertBefore('head title'),$(`<center><div><marquee direction="right" width="99%" id="asim8" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">  &nbsp;اهلا وسهلا بكم في شات <font color="#595757" class="asim12">المحبه </font> ● · افضل سوبر لهذا الاسبوع  · ●<font class="asim11" color="e#727A54"> قريبآ </font> ● · افضل زائر لهذا الاسبوع  · ●<font class="asim11" color="#ff9900"> قريبآ </font> ● · افضل زائرة لهذا الاسبوع  · ●<font class="asim11" color="#ff00ff"> قريبآ </font> ● · مبدع الحائط لهذا الاسبوع · ●<font class="asim11" color="#5CAD9A"> قريبآ </font> ● · مبدعة الحائط لهذا الاسبوع · ●<font class="asim11" color="#800000"> قريبآ </font> ● · افضل تواجد لهذا الاسبوع · ●<font class="asim11" color="#417570"> قريبآ </font>  ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break'),document.getElementById('tbox').placeholder='\u0639\u0645\u064A \u0639\u0648\u0641 \u0627\u0644\u0633\u0637\u062D \u0648\u062F\u0631\u062F\u0634 \u0648\u064A\u0627\u0646\u0627',$('.fl.ustat').css('width','5px'),$('.fa.fa-user-plus').html(`تسجيل`),document.getElementById('u1').placeholder='\u0627\u0643\u062A\u0628 \u0627\u0633\u0645\u0643 \u064A\u0632\u0627\u064A\u0631.\u0627\u0634\u0631\u0628 \u062C\u0627\u064A \u064A\u0632\u0627\u064A\u0631',document.getElementById('pass1').placeholder='\u0628\u0627\u0633\u0648\u0648\u0631\u062F\u0643',document.getElementById('u2').placeholder='\u0627\u0633\u0645 \u0639\u0636\u0648\u064A\u062A\u0643',document.getElementById('pass2').placeholder='\u0628\u0627\u0633\u0648\u0648\u0631\u062F\u0643 \u0628\u0633 \u0644\u062A\u0646\u0633\u0649',document.getElementById('u3').placeholder='\u0627\u0633\u0645\u0643 \u0644\u0627\u0632\u0645 \u062A\u062D\u0641\u0638\u0647';
+
+$('.dpnl').append(`   <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> 
+          <center><iframe src="https://mixlr.com/kjshbsk100gmailcom/embed?color=7A7A7A&artwork=false" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe></center>
+      </div>`);
+$('#d0').append(`<label title="الإذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+        data-toggle="tab" data-target="#mic" class="ae fa label label-primary fa-microphone ">الاذاعة</label>`)
+
 													}
 														catch (e){
 															console.log(e)
@@ -26,56 +25,4 @@ $(`<center><font size="4" color="#C74653"> <marquee direction="right" width="100
 													function saveColor(){
 														var dfsdfsdf = $('.label-primary, .btn-primary').css('background-color');
 														console.log(dfsdfsdf)
-														var colorLo = {bgcolor:$('.bgcolor').val(),btcolor:$('.btcolor').val(),bocolor:$('.bocolor').val(),hicolor:$('.hicolor').val()}
-														localStorage.setItem('colorLo', JSON.stringify(colorLo));
-														getLoColor(JSON.stringify(colorLo))
-														$('.divColorLo').slideToggle();
-													}
-													var hexDigits = new Array
-													("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
-													function rgb2hex(rgb) {
-														rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-														return  hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-													}
-													function hex(x) {
-														return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
-													}
-													function getLoColor(c){
-														var lo = localStorage.getItem('colorLo')||c;
-														if(lo){
-															var stLoc = localStorage.getItem('colorSt');
-															var loJs = JSON.parse(lo)
-															for(var i in loJs){
-																$('.'+i).val(loJs[i]);
-																switch(i){
-																	case 'bgcolor':
-																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.bg').css('background-color'))
-																		break;
-																	case 'btcolor':
-																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.label-primary, .btn-primary').css('background-color'))
-																		break;
-																	case 'bocolor':
-																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.border').css('border-color'))
-																		break;
-																	case 'hicolor':
-																		if(loJs[i]==="000000")loJs[i] = rgb2hex($('.light').css('background-color'))
-																		break;
-																}
-															}
-															var aa = '<style class="colorLo">.border{border-color: #'+loJs.bocolor+'!important;} .primaryborder{border-color: #'+loJs.btcolor+'!important;} .label-primary, .btn-primary {background-color: #'+loJs.btcolor+'!important;} .light{background-color: #'+loJs.hicolor+'!important;} .bg{background-color: #'+loJs.bgcolor+';}</style>';
-															if(stLoc && !c)aa = stLoc;
-															localStorage.setItem('colorSt', aa);
-															var loHtml = $('.colorLo')
-															if(loHtml.length >0)loHtml.text($(aa).text());
-															else $('head').append(aa);
-														}else{
-															$('.colorLo').remove();
-															$('.bgcolor,.btcolor,.hicolor,.bocolor').val('000000');
-															$('.bgcolor,.btcolor,.hicolor,.bocolor').css('background-color','#000000');
-														}
-													}
-													getLoColor()
-													$('.ssss').click(function(e){
-														e.stopPropagation();
-														$('.divColorLo').slideToggle();
-													})
+														var colorLo = {bgcolor:$('.bgcolor').val(),btcolor:$('.btcolor').val(),bocolor:$('.bocolor').val(),hicolor
